09291e976fd90c89eeb481aecb3d5e289fa4e2a3
SpaceColony <jovfernando@yahoo.com>
2019-09-25 09:18:29 +0700

    end first practice

--- App.js
+++ App.js
@@ -6,11 +6,17 @@ export default class AwesomeApp extends Component {
 
 
     return (
-      <View style={{backgroundColor: '#ffffff', flex: 1, paddingTop: 50, paddingHorizontal: 20, flexDirection: "row"}}>
-        <View style={{backgroundColor: '#f1c40f', flex: 1, height: 80, marginHorizontal: 5, borderRadius: 10}}></View>
-        <View style={{backgroundColor: '#3498db', flex: 1, height: 80, marginHorizontal: 5, borderRadius: 10}}></View>
-        <View style={{backgroundColor: '#2ecc71',flex: 1, height: 80, marginHorizontal: 5, borderRadius: 10}}></View>
-        <View style={{backgroundColor: '#3498db',flex:1, height: 80, marginHorizontal: 5, borderRadius: 10}}></View>
+      <View style={styles.parent}>
+        <View style={{backgroundColor: '#f1c40f', width: '100%', height: 100, borderRadius: 10, flexDirection: "row", padding: 5, marginBottom: 10,  position: 'relative'}}>
+          <View style={{backgroundColor: 'red', width: 25, height: 25, borderRadius: 100, justifyContent: 'center', position: 'absolute', right: -10, top: -10}}>
+            <Text style={{color: 'white', fontWeight: 'bold', textAlign: 'center'}}>1</Text>
+          </View>
+          </View>
+        <View style={{backgroundColor: '#3498db', flex: 1, height: 80, marginRight: 5, borderRadius: 10, justifyContent: 'center'}}>
+          <Text style={{color: 'white', fontStyle: 'italic', fontWeight: 'bold', textAlign: 'center'}}>HUBA</Text>
+        </View>
+        <View style={[{backgroundColor: '#2ecc71', marginHorizontal: 5}, styles.box]}></View>
+        <View style={[{backgroundColor: '#3498db', marginLeft: 5}, styles.box]}></View>
         
     
       </View>
@@ -19,6 +25,19 @@ export default class AwesomeApp extends Component {
 }
 
 const styles = StyleSheet.create({
+  parent: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    flexWrap: 'wrap'
+  },
 
+  box: {
+    flex: 1,
+    height: 80,
+    borderRadius: 10
+  }
 
 })
